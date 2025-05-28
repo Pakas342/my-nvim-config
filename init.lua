942957b0dfce22af1f2daac69a672bfa1ff73cbd
Juan Barrera <jcbp1999@gmail.com>
2025-05-28 10:30:39 +1000

    updated avante

--- init.lua
+++ init.lua
@@ -1063,14 +1063,24 @@ require('lazy').setup({
   {
     'yetone/avante.nvim',
     event = 'VeryLazy',
-    lazy = false,
-    version = false,
+    version = false, -- Never set this value to "*"! Never!
     opts = {
       -- add any opts here
+      -- for example
+      provider = 'openai',
+      openai = {
+        endpoint = 'https://api.openai.com/v1',
+        model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_completion_tokens = 16000, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false',
+    build = 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false', -- for windows
     dependencies = {
+      'nvim-treesitter/nvim-treesitter',
       'stevearc/dressing.nvim',
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',

c60605387db142045304e8104638eb7e63a3d2d5
Juan Barrera <jcbp1999@gmail.com>
2025-07-15 12:21:02 +1000

    now we're using the agentic mode as it's fixed

--- init.lua
+++ init.lua
@@ -1189,7 +1189,7 @@ require('lazy').setup({
       auto_suggestions_provider = 'openai',
       provider = 'openai', -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
       ---@alias Mode "agentic" | "legacy"
-      mode = 'legacy', -- The default mode for interaction. "agentic" uses tools to automatically generate code, "legacy" uses the old planning method to generate code.
+      mode = 'agentic', -- The default mode for interaction. "agentic" uses tools to automatically generate code, "legacy" uses the old planning method to generate code.
       -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
       -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
       -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.

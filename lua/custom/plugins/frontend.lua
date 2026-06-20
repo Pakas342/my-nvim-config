50396c02a1727099ce6a17a20af487f41ce97a28
Pakas342 <jcbp1999@gmail.com>
2026-06-20 18:27:13 +1000

    html closing improved

--- lua/custom/plugins/frontend.lua
+++ lua/custom/plugins/frontend.lua
@@ -8,24 +8,8 @@ return {
   -- Auto-close and rename HTML/JSX tags
   {
     'windwp/nvim-ts-autotag',
-    dependencies = 'nvim-treesitter/nvim-treesitter',
     event = 'VeryLazy',
-    config = function()
-      require('nvim-ts-autotag').setup {
-        opts = {
-          -- Defaults
-          enable_close = true, -- Auto close tags
-          enable_rename = true, -- Auto rename pairs of tags
-          enable_close_on_slash = false, -- Auto close on trailing </
-        },
-        -- Also override individual filetype configs
-        per_filetype = {
-          ['html'] = {
-            enable_close = false,
-          },
-        },
-      }
-    end,
+    opts = {},
     ft = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'vue', 'xml' },
   },
 }
